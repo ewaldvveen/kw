557c32de68e9d6cae64029559115179143e00169
Ewald van Veen <ewaldvanveen@gmail.com>
2022-09-26 21:29:42 +0200

    Add svg logo to header

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -1,4 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { HeaderComponent } from './header.component';
 
@@ -20,4 +22,23 @@ describe('HeaderComponent', () => {
   it('should create the HeaderComponent', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('HTML elements', () => {
+    it('should contain an h1-element with the title of the application', () => {
+      const titleDebugElement: DebugElement = fixture.debugElement;
+      const h1DebugElement = titleDebugElement.query(By.css('h1'));
+      const h1Element: HTMLElement = h1DebugElement.nativeElement;
+
+      expect(h1Element).toBeDefined();
+      expect(h1Element.textContent).toEqual('kwisr.');
+    });
+
+    it('should contain a logo', () => {
+      const logoDebugElement: DebugElement = fixture.debugElement;
+      const svgDebugElement = logoDebugElement.query(By.css('svg'));
+      const svgElement: HTMLElement = svgDebugElement.nativeElement;
+
+      expect(svgElement).toBeDefined();
+    });
+  })
 });
